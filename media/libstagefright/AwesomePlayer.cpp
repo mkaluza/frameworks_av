ac2d2c0ff8be47f9f30d7b81d7913ee93109291c
nuclearmistake <nuclearmistake@gmail.com>
2014-07-22 01:55:44 -0400

    Revert "Revert "Avoid jumps to faulty position after seeks""
    This reverts commit 9373ddf03ec4b5be95f602c401646c60f8fdb26c.

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -2686,7 +2686,10 @@ void AwesomePlayer::onCheckAudioStatus() {
             mSeekNotificationSent = true;
         }
 
-        mSeeking = NO_SEEK;
+        if (mVideoSource == NULL) {
+            // For video the mSeeking flag is always reset in finishSeekIfNecessary
+            mSeeking = NO_SEEK;
+        }
 
         notifyIfMediaStarted_l();
     }
