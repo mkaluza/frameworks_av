8050f0e8176abf9c70085a8695f751b3700c824f
Christopher R. Palmer <crpalmer@gmail.com>
2014-09-16 11:20:46 -0400

    stagefright: Allow devices to use older widevine blobs
    Many devices have widevine plugs build with an older MediaSource
    interface.  Trying to use them fails when using secure buffers
    because the newer library ends up calling the destructor instead
    of setBuffers.  Provide an #ifdef to accommodate these older
    blobs.
    
    Makes it possible to fix JIRA-4944 on hammerhead.
    
    Change-Id: I4ce24a16f3eceab0c4def4b09850ed569eb392ab

--- include/media/stagefright/MediaSource.h
+++ include/media/stagefright/MediaSource.h
@@ -64,7 +64,9 @@ struct MediaSource : public virtual RefBase {
     virtual status_t read(
             MediaBuffer **buffer, const ReadOptions *options = NULL) = 0;
 
+#ifndef WIDEVINE_PLUGIN_PRE_NOTIFY_ERROR
     virtual void notifyError(status_t err) {}
+#endif
 
 #ifdef SEMC_ICS_CAMERA_BLOB
     virtual status_t getRecordingBuffer(unsigned int index, sp<MemoryBase>** buffer);
@@ -126,6 +128,11 @@ protected:
 private:
     MediaSource(const MediaSource &);
     MediaSource &operator=(const MediaSource &);
+
+#ifdef WIDEVINE_PLUGIN_PRE_NOTIFY_ERROR
+public:
+    virtual void notifyError(status_t err) {}
+#endif
 };
 
 }  // namespace android
